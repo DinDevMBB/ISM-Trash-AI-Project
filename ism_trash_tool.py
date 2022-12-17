fe63a6a0fac692455400d6c2b297f9917df422af
1Dragon-Lord1 <subudhi.sohan@gmail.com>
2022-12-17 10:59:34 -0600

    Update ism_trash_tool.py

--- ism_trash_tool.py
+++ ism_trash_tool.py
@@ -3,7 +3,34 @@
 
 # In[ ]:
 
+import io
 
+import numpy as np
+import requests
 import streamlit as st
-st.title('Waste Prediction Tool')
+
+st.title("Environmental Waste Detection Through Neural Networks")
+st.markdown(
+    "### Predict detected waste and littering in an image",
+    unsafe_allow_html=True,
+)
+
+with open("Image #1-A.jpg", "rb") as f:
+    st.image(f.read(), use_column_width=True)
+
+st.text("Upload an image to recieve output")
+
+from PIL import Image
+
+
+def main():
+    img_file = st.file_uploader("Upload an image", type=["jpg", "png"])
+    if img_file is not None:
+
+        with st.spinner("Predicting..."):
+            st.success(f"Your file has been uploaded and processed successfully!")
+
+
+if __name__ == "__main__":
+    main()
 
